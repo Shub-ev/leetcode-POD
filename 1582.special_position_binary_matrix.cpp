4250bdbe8beec85785c2caf1c9ef7efbf00d1843
Shub-ev <shubhamg200204@gmail.com>
2026-03-04 12:31:29 +0530

    use matrix traversal || my version && cleaner version

--- 1582.special_position_binary_matrix.cpp
+++ 1582.special_position_binary_matrix.cpp
@@ -38,3 +38,30 @@ public:
         return res;
     }
 };
+
+
+// Also another solution -> not optimal but cleaner
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<int> row(m, 0), col(n, 0);
+
+        // Here we precount the number of 1's in rows and cols
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (mat[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
+                }
+
+        int ans = 0;
+        // Then iterate matrix again to find the special positions
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1)
+                    ans++;
+
+        return ans;
+    }
+};
